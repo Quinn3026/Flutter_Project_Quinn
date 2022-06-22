e35057d1293545728c2689cd10934a84737615cc
Quinn3026 <gacayanquinn22222@gmail.com>
2022-06-22 09:30:13 +0800

    1st Commit

---
+++ destini_proj_app/android/app/src/main/kotlin/quinn/com/destini_proj_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package quinn.com.destini_proj_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ destini_proj_app/lib/story.dart
@@ -0,0 +1,15 @@
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({required this.storyTitle,
+    required  this.choice1,
+    required this.choice2}){
+
+    storyTitle = this.storyTitle;
+    choice1 = this.choice1;
+    choice2 = this.choice2;
+  }
+}
